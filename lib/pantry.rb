93ffe9e663c6c35badc03449b8b34bb6c0214a1d
David Gomez <davdg25@gmail.com>
2019-07-30 11:39:10 -0600

    stuck on iteration 3

--- lib/pantry.rb
+++ lib/pantry.rb
@@ -13,13 +13,21 @@ end
   end
 
   def enough_ingredients_for_recipe(recipe)
+
+    # ingredients_required
+
     # if stock_check(ingredient)
     # stock_check(ingredieng) >=
 
     # stock_check >= ingredients_amount
 
-      recipe.amount_required[ingredient].each do |ingredient, quantity|
-      stock_check(ingredient) >= quantity
+    recipe.ingredients_required.each do |ingredient, quantity|
+   stock_check(ingredient) >= quantity
+
+      # recipe.amount_required[ingredient].each do |ingredient, quantity|
+      # stock_check(ingredient) >= quantity
+
+      #all?,
     end
   end
 end
